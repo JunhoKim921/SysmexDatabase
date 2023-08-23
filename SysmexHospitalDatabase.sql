546ac08c497edaecb4397467cb201cbf98d1c54a
JunhoKim921 <142218084+JunhoKim921@users.noreply.github.com>
2023-08-23 16:06:34 +1200

    SysmexHospitalDatabase v3

--- SysmexHospitalDatabase.sql
+++ SysmexHospitalDatabase.sql
@@ -1,5 +1,5 @@
-create database SysmexHospital;
-use SysmexHospital;
+create database SysmexHospitalFinal;
+use SysmexHospitalFinal;
 
 CREATE TABLE IF NOT EXISTS Referee (
 	refereeID INT PRIMARY KEY NOT NULL,
@@ -25,32 +25,54 @@ CREATE TABLE IF NOT EXISTS Patient (
 	NHI CHAR(7),
 	firstName VARCHAR(25) NOT NULL,
 	lastName VARCHAR(25) NOT NULL,
-	birthdate DATE NOT NULL,
-	patAgeAtRef INT NOT NULL,
-	gender VARCHAR(6)
+	birthdate DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Referral (
     refID INT PRIMARY KEY NOT NULL,
     refDate DATE NOT NULL,
     healthTarget VARCHAR(3),
-    refFrom VARCHAR(20),
+    daysFromRef INT,
+    patAgeAtRef INT,
     patientID INT NOT NULL,
     refereeID INT NOT NULL,
     surgeonID INT NOT NULL,
-    FOREIGN KEY (patientID) REFERENCES patient (patientID),
-    FOREIGN KEY (refereeID) REFERENCES referee (refereeID),
-    FOREIGN KEY (surgeonID) REFERENCES surgeon (surgeonID)
+    FOREIGN KEY (patientID) REFERENCES Patient (patientID),
+    FOREIGN KEY (refereeID) REFERENCES Referee (refereeID),
+    FOREIGN KEY (surgeonID) REFERENCES Surgeon (surgeonID)
 );
 
 CREATE TABLE IF NOT EXISTS Waitlist (
 	waitlistID INT PRIMARY KEY NOT NULL,
 	waitlistDate DATE NOT NULL,
 	fsaDate DATE NULL,
-	daysWaitingFromRef INT,
 	refID INT NOT NULL,
 	FOREIGN KEY (refID) REFERENCES referral (refID)
 );
+
+-- Update daysFromRef and patAgeAtRef in the Referral table
+UPDATE Referral
+SET
+    daysFromRef = DATEDIFF(COALESCE(
+        (SELECT fsaDate FROM Waitlist WHERE refID = Referral.refID),
+        CURDATE()
+    ), refDate),
+    patAgeAtRef = (
+        SELECT YEAR(refDate) - YEAR(p.birthdate)
+        FROM Patient p
+        WHERE p.patientID = Referral.patientID
+    )
+WHERE
+    daysFromRef IS NULL OR patAgeAtRef IS NULL;
+
+Delete FROM referral where refID = 389;
+Delete FROM waitlist where waitlistID = 389;
+
+select * from waitlist;
+select * from referral;
+
+drop table waitlist;
+drop table referral;
 
 LOAD DATA INFILE 'C:/Temp/RefereeData.csv'
 INTO TABLE referee
@@ -87,13 +109,9 @@ INTO TABLE waitlist
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(waitlistID, waitlistDate, @fsaDate, @daysWaitingFromRef, refID) -- Use @daysWaitingFromRef to hold the value temporarily
-SET fsaDate = NULLIF(@fsaDate, ''), -- Set fsaDate to NULL if @fsaDate is an empty string
-    daysWaitingFromRef = NULLIF(@daysWaitingFromRef, ''); -- Set daysWaitingFromRef to NULL if @daysWaitingFromRef is an empty string
+(waitlistID, @waitlistDate, @fsaDate, refID) -- Use @waitlistDate and @fsaDate to hold the values temporarily
+SET waitlistDate = IF(@waitlistDate = '', '1970-01-01', @waitlistDate),
+    fsaDate = NULLIF(@fsaDate, ''); -- Set fsaDate to NULL if @fsaDate is an empty string
 
-drop table waitlist;
-drop table referral;
-drop table patient;
-drop table surgeon;
-drop table department;
-drop table referee;
+
+
